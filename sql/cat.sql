1347721d597c71ce974bb9541a63f5c3f2914e66
287092313 <287092313@qq.com>
2020-02-16 21:02:18 +0800

    测试cat 建表语句

---
+++ sql/cat.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `cat` (
+  `id` int(11) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `name` varchar(11) DEFAULT NULL,
+  `catAge` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
